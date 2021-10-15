eb5d27bec43d1706b31352ac6eeb98ad96212df7
Vezeveer <mortalmanny@live.com>
2021-10-15 16:37:22 +0800

    post-final

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment; //try change to android.support.v4.app
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,9 @@ public class CrimeFragment extends Fragment {
                 container, false);
 
         mTitleField = v.findViewById(R.id.edit_text_title);
+        mDateButton = v.findViewById(R.id.edit_text_date);
+        mDateButton.setText(mCrime.getmDate()+"");
+        mDateButton.setEnabled(false);
 
         return v;
     }
