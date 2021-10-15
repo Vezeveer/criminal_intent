29d8cadaaa4cea51200c2e9d5cea596ee0daee8c
Vezeveer <mortalmanny@live.com>
2021-10-15 15:35:27 +0800

    Post-Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.criminalintent;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment; //try change to android.support.v4.app
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime,
+                container, false);
+
+        mTitleField = v.findViewById(R.id.edit_text_title);
+
+        return v;
+    }
+
+
+}
