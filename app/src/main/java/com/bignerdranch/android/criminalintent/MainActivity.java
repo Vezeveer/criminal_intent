27e65027210b78fd77e51f7d462a7036f4aa55a6
Vezeveer <mortalmanny@live.com>
2021-10-27 21:44:45 +0800

    Added SingleFragmentActivity 8.8

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
@@ -1,29 +1,12 @@
 package com.bignerdranch.android.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if(fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment(){
+        return new CrimeFragment();
     }
+
 }
