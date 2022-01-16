2a05de5645513474668386940d44502903bebbbe
Vezeveer <mortalmanny@live.com>
2022-01-17 00:05:53 +0800

    Added SQLite Database

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -46,6 +46,13 @@ public class CrimeFragment extends Fragment {
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater,
